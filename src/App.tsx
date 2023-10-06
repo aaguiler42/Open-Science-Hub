b6abb16292cd0992c2fbfdf9972138fc28a7a89e
Narciso González Calderón <narsodev@gmail.com>
2023-10-06 21:20:45 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ import "./App.css";
 const config: NeovisConfig = {
   containerId: "viz",
   neo4j: {
-    serverUrl: "bolt://2bb21514databases.neo4j.io:7687",
+    serverUrl: "neo4j://2bb21514databases.neo4j.io:7687",
     serverUser: "neo4j",
     serverPassword: "m5-u_fQxwOCH4Ka7-iBIwaUKXSoIkRldnMRP3bRVrmk",
     driverConfig: {
-      encrypted: false,
-      trust: "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES",
+      encrypted: true,
+      trust: "TRUST_CUSTOM_CA_SIGNED_CERTIFICATES",
     }
   },
   labels: {
