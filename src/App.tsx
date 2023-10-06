18080477cbe0ad614f30f4fd30f9d25612e1f1ed
Narciso González Calderón <narsodev@gmail.com>
2023-10-06 21:10:04 +0200

    Add files

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import NeoVis, {NeovisConfig} from "neovis.js";
+
+
+
+import "./App.css";
+
+const config: NeovisConfig = {
+  containerId: "viz",
+  neo4j: {
+    serverUrl: "neo4j://2bb21514databases.neo4j.io:7687",
+    serverUser: "neo4j",
+    serverPassword: "m5-u_fQxwOCH4Ka7-iBIwaUKXSoIkRldnMRP3bRVrmk",
+    driverConfig: {
+      encrypted: true,
+      trust: "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES",
+    }
+  },
+  labels: {
+    contributor: {
+      label: "name",
+    }
+  },
+  relationships: {},
+};
+
+function App() {
+  const neoViz = new NeoVis(config);
+  neoViz.render();
+
+  return (
+    <div>
+      <h1>Neo4j React App</h1>
+      <div id="viz"></div>
+    </div>
+  );
+}
+
+export default App;
