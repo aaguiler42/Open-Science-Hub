3a1a95ba32d5ca08cdef74c8bc663ce18c5a8cf2
Narciso González Calderón <narsodev@gmail.com>
2023-10-07 11:29:38 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,75 @@
 // import vis from "vis-network";
-import {
-  Network,
-  Options,
-  Edge,
-  Node,
-  Data
-} from "vis-network/standalone/esm/vis-network";
-
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-const nodes: Node[] = [
-  { id: 1, label: "Node 1" },
-  { id: 2, label: "Node 2" },
-  { id: 3, label: "Node 3" },
-  { id: 4, label: "Node 4" },
-  { id: 5, label: "Node 5" },
-];
-
-// create an array with edges
-const edges: Edge[] = [
-  { from: 1, to: 3 },
-  { from: 1, to: 2 },
-  { from: 2, to: 4 },
-  { from: 2, to: 5 },
-];
-
-const data: Data = {
-  nodes: nodes,
-  edges: edges,
-};
+import { nodes, edges } from "./data/graph";
+import Graph from "./components/Graph";
+import { NodeData } from "./types";
 
 function App() {
-  const [selected, setSelected] = useState<Node | null>(null);
+  const [selected, setSelected] = useState<NodeData | null>(null);
 
-  useEffect(() => {
-    const container = document.getElementById("mynetwork");
-    const options: Options = {
-      clickToUse: true,
-      nodes: {
-        shape: 'circle',
-        color: {
-          background: "#ffffff",
-          border: "#1c1c1c",
-          highlight: {
-            background: "#ffffff",
-            border: "#000000",
-          },
-          hover: {
-            background: "#ffffff",
-            border: "#000000",
-          },
-        }
-      },
-      edges: {
-        color: {
-          color: "#8c8c8c",
-          highlight: "#000000",
-          hover: "#000000",
-        }
-      }
-    };
-    const network = new Network(container, data, options);
-
-
-    network.on("click", function (params) {
-      console.log(params)
-      if (params.nodes.length) setSelected(nodes?.find(({id}) => id ===params.nodes[0]) ?? null)
-    })
-
-    const obj = {
-      viewPosition: network.getViewPosition(),
-      scale: network.getScale(),
-      positions: network.getPositions(),
-    }
-    
-
-  }, []);
-  console.log(selected)
   return (
-    <div
-      style={{
-      }}
-    >
+    <div style={{
+    }}>
+      <nav style={{
+        display: "flex",
+        height: "4vh",
+        padding: ".5rem",
+        borderBottom: "1px solid #fff"
+      }}>
+        <span>OSH</span>
+
       <h1>Neo4j React App</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      </nav>
       <div
         style={{
           display: "grid",
           gridTemplateColumns: "1fr 4fr",
-          height: "100%",
+          height: "92vh",
           overflow: "hidden",
         }}
       >
-        <div style={{
-          border: "1px solid white",
-        }}></div>
-        <div id="mynetwork" style={{
-          height: "80vh",
-          border: "1px solid white",
-        }}></div>
+        <div
+          style={{
+            border: "1px solid white",
+            textAlign: "left",
+            overflowY: "auto",
+            height: "100%"
+          }}
+        >
+          {nodes.map((node) => (
+            <button key={node.id} style={{
+              height: "5rem",
+              display: "flex",
+              flexDirection: "column",
+              alignItems:"center",
+              width: "100%",
+              textAlign: "left",
+            }}
+            onClick={() => setSelected(node)}
+            >
+              <div style={{
+                width: "100%",
+                height: "70%",
+                backgroundColor: "red",
+              }}></div>
+              <h3 style={{
+                fontWeight: "bold",
+              }}>{node.label}</h3>
+              <p style={{
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                width: "20px",
+                whiteSpace: "nowrap",
+              }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod at ducimus officia sed, nihil dicta voluptatum blanditiis, voluptatem ad voluptate odit, nemo similique consequuntur a atque molestiae quaerat alias ipsam!</p>
+            </button>
+          ))}
+        </div>
+        <Graph height="96vh" nodes={nodes} edges={edges} />
       </div>
     </div>
   );
